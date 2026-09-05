7f9a9f0a68c290be07f75964770c369982048ad1
Ummadisetti Sindhuja <ummadisettisindhuja2@gmail.com>
2026-09-05 14:38:50 -0500

    Add sample Python application

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return jsonify(
+        message="End-to-End CI/CD Pipeline is running",
+        environment=os.getenv("APP_ENV", "development")
+    )
+
+@app.get("/health")
+def health():
+    return jsonify(status="healthy"), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
